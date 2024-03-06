4822dbce21e047897b1b2e49e2ba17e55803c0c6
Iliya Popov <popov_iliya@yahoo.com>
2024-03-06 09:20:13 +0200

    First version

---
+++ Default.aspx
@@ -0,0 +1,43 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ProjectDb._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="row">
+        <div class="col-md-4">
+            <h2>User List</h2>
+             <form ID="form1" action="/" method="post">
+                 <div class="row">
+                    From date: <asp:TextBox ID="fromDateSearch" runat="server" ></asp:TextBox>
+                    To date: <asp:TextBox ID="toDateSearch" runat="server" ></asp:TextBox>
+                     <asp:Button ID="SerchBtn" runat="server" Text="Search" OnClick="SerchBtn_Click" />
+                     
+                     <asp:GridView runat="server" ID="usersGrView" AutoGenerateColumns="false" CellPadding="4" 
+                         AllowPaging="true" OnPageIndexChanging="OnPageIndexChanging" PageSize="10">
+                     <AlternatingRowStyle BackColor="White" ForeColor="#284775" />
+                     <EditRowStyle BackColor="#999999" />
+                     <FooterStyle BackColor="#5d7b9d" Font-Bold="true" ForeColor="White" />
+                     <HeaderStyle BackColor="#5d7b9d" Font-Bold="true" ForeColor="White" HorizontalAlign="Center" />
+                     <PagerStyle BackColor="#284775" ForeColor="White" HorizontalAlign="Center" />
+                     <RowStyle BackColor="#f7f6f3" ForeColor="#333333" Height="50" Width="75" />
+                     <SelectedRowStyle BackColor="#f7f6f3" Font-Bold="true" ForeColor="#333333" />
+                         <Columns>
+                              <asp:BoundField DataField="Id" HeaderText="User Id" />
+                              <asp:BoundField DataField="firstName" HeaderText="First Name" />
+                              <asp:BoundField DataField="lastName" HeaderText="Last Name" />
+                              <asp:BoundField DataField="userEmail" HeaderText="Email" />
+                         </Columns>
+                     </asp:GridView>
+                 </div>
+                 <div class="col-md-4">
+                     <asp:Button ID="InitializationDbBtn" runat="server" Text="Initialization DB" OnClick="InitializationDbBtn_Click" />
+                 </div>
+             </form>
+        </div>
+
+        <div id="chart_conteiner" class="col-md-4">
+            <h2>Google Chart</h2>
+            
+        </div>
+    </div>
+
+</asp:Content>
---
+++ Default.aspx.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ProjectDb
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                BindGridView();
+            }
+        }
+
+        protected void BindGridView()
+        {
+            try
+            {
+                sqlConnection = new SqlConnection(constr);
+                sqlConnection.Open();
+                sqlCommand = new SqlCommand("SELECT * from Users");
+                sqlDataAdapter = new SqlDataAdapter();
+                sqlCommand.Connection = sqlConnection;
+                sqlDataAdapter.SelectCommand = sqlCommand;
+                dataTable = new DataTable();
+                sqlDataAdapter.Fill(dataTable);
+                this.usersGrView.DataSource = dataTable;
+                this.usersGrView.DataBind();
+                sqlConnection.Close();
+            }
+            catch (Exception ms)
+            {
+                //To do
+            }
+        }
+        protected void OnPageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            usersGrView.PageIndex = e.NewPageIndex;
+            this.BindGridView();
+        }
+
+        protected void SerchBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                sqlConnection = new SqlConnection(constr);
+                sqlConnection.Open();
+                sqlCommand = new SqlCommand("SELECT * FROM Users,TimeLog WHERE LogDate BETWEEN'" + this.fromDateSearch.Text + "'AND'" + this.toDateSearch.Text + "'");
+                sqlCommand.Connection = sqlConnection;
+                dataTable = new DataTable();
+                sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+                sqlDataAdapter.Fill(dataTable);
+                this.usersGrView.DataSource = dataTable;
+                this.usersGrView.DataBind();
+                sqlConnection.Close();
+            }
+            catch (Exception ms)
+            {
+                // To do
+            }
+        }
+        protected void InitializationDbBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                sqlConnection = new SqlConnection(constr);
+                sqlConnection.Open();
+                sqlCommand = new SqlCommand("InitializeDatabase", sqlConnection);
+                sqlCommand.Connection = sqlConnection;
+                sqlCommand.ExecuteNonQuery();
+                sqlConnection.Close();
+                Response.Redirect("Default.aspx");
+            }
+            catch(Exception ms)
+            {
+                //To do
+            }
+        }
+        private SqlConnection sqlConnection;
+        private SqlCommand sqlCommand;
+        private SqlDataAdapter sqlDataAdapter;
+        private DataTable dataTable;
+        const string constr = "Data Source=localhost;Initial Catalog=projectDb;Integrated Security=True";
+    }
+}
